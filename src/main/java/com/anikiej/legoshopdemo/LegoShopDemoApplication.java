1af4397192dc36e98cf2c9d953217bcafd830bbe
AnakinM <mateusz.anikiej@gmail.com>
2022-01-21 16:08:05 +0100

    Initial commit

---
+++ src/main/java/com/anikiej/legoshopdemo/LegoShopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.anikiej.legoshopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoShopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoShopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anikiej/legoshopdemo/LegoShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anikiej.legoshopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoShopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
